6004125da6c01bfc6a5afc84a440d00b8b5fceeb
VladPrickly <vlad.prickly@gmail.com>
2025-09-07 23:26:54 +0300

    Add files via upload

---
+++ application/db/people.py
@@ -0,0 +1,2 @@
+def get_employees(employee_amount):
+    print(f'Количество сотрудников предприятия: {employee_amount} человек')
---
+++ application/salary.py
@@ -0,0 +1,2 @@
+def calculate_salary(month_salary, month_num):
+    print(f'Заработная плата за {month_num} месяцев составляет {month_salary * month_num} рублей')
